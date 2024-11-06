df53af899570e45b8351f76966805cc6f986b912
Paulo Cesar <paulocesardearaujopaulo@gmail.com>
2024-11-05 21:38:47 -0300

    BackEnd Clientes Pronto

---
+++ src/service/Servico2/alterarCliente.js
@@ -0,0 +1,8 @@
+import { alterar } from "../../repository/servico2Repository.js";
+
+export default async function alterarCliente(id, pessoa) {
+
+    let linhasAfetadas = await alterar(id, pessoa);
+    return linhasAfetadas;
+}
+
